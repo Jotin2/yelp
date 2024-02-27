b9de4aecff6a052669e7863206f6a6e8fe3be241
Joshua Asari <josh.asari@gmail.com>
2024-02-27 15:04:58 -0500

    Added Skeleton Routes

--- server/server.js
+++ server/server.js
@@ -1,7 +1,65 @@
-const express = require("express")
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+const app = express();
 
-const app = express()
+// Middleware
+app.use(express.json());
 
-app.listen(3000, () => {
-    console.log("server is up and listening on port 3000")
-})
+// Get all Restaurants
+app.get("/api/v1/restaurants", (req, res) => {
+    res.status(200).json({
+        status: "success",
+        data: {
+            restaurant: ["McDonalds", "Wendys"]
+        }
+    });
+});
+
+// Get a Restaurant
+app.get("/api/v1/restaurants/:id", (req, res) => {
+    console.log(req.params);
+    res.status(200).json({
+        status: "success",
+        data: {
+            restaurant: ["McDonalds"]
+        }
+    });
+});
+
+
+// Create a Restaurant
+app.post("/api/v1/restaurants", (req, res) => {
+    console.log(req.body);
+    res.status(201).json({
+        status: "success",
+        data: {
+            restaurant: ["McDonalds"]
+        }
+    });
+});
+
+// Update a Restaurant
+app.put("/api/v1/restaurants/:id", (req, res) => {
+    console.log(req.params.id);
+    console.log(req.body);
+    res.status(200).json({
+        status: "success",
+        data: {
+            restaurant: ["McDonalds"]
+        }
+    });
+});
+
+// Delete a Restaurant
+app.delete("/api/v1/restaurants/:id", (req, res) => {
+    res.status(204).json({
+        status: "success"
+    });
+});
+
+const port = process.env.PORT || 3001;
+
+app.listen(port, () => {
+    console.log(`server is up and listening on port ${port}`)
+});
